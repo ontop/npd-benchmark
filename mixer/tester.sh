daecad1c856acced88ffbf9ee419d263a756e6ec
tirrolo <tirrolo@users.noreply.github.com>
2014-06-06 18:40:40 +0200

    Update tester.sh
    Removal of useless lines in tester.sh

--- mixer/tester.sh
+++ mixer/tester.sh
@@ -1,33 +1,5 @@
 #!/bin/sh
 
-OWLDIRECTORY=src/main/resources/davide
-
-setup () {
-    OBDAFILE=$OWLDIRECTORY/npd-v2-ql_a.obda
-    
-    LINE=`grep 'jdbc:mysql' $OBDAFILE`
-
-    ## Replace the line in the file
-    
-    NEW_URL='jdbc:mysql://10.7.39/npd_1'
-    
-    OLD_URL=`echo $LINE | awk '{print $2}'`
-    OLD_URL=`escape_sed $OLD_URL`
-    NEW_URL=`escape_sed $NEW_URL`
-    echo "s/$OLD_URL/$NEW_URL/g"
-    sed "s/$OLD_URL/$NEW_URL/g" $OBDAFILE > ${OBDAFILE}_1
-    
-}
-
-# escapestr_sed()
-# read a stream from stdin and escape characters in text that could be interpreted as
-# special characters by sed
-escape_sed() {
- echo $1 | sed \
-  -e 's/\//\\\//g' \
-  -e 's/\&/\\\&/g'
-}
-
 do_test () {
     for i in 1 2 3 4 5 6 7 8
     do
