28b713e644ee74ba9e1e093b85a567a223e09200
tirrolo <tirrolo@users.noreply.github.com>
2014-06-06 18:44:41 +0200

    Update tester.sh
    Add comments in tester.sh

--- mixer/tester.sh
+++ mixer/tester.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
+
+# Specify here the tests that need to be performed as a number in the for cycle.
+# 
+#
+#
+
 do_test () {
-    for i in 1 2 3 4 5 6 7 8
+    for i in 1 2 3 4 5 6 7 8 # Test with parameters in folders 1 ... 8
     do
-	DBNAME=`cat $i/dbName`
+	DBNAME=`cat $i/dbName` # Read the database name
 	echo $DBNAME
-	java -jar Mixer.jar $DBNAME 10 "$i/npd-v2-ql_a.owl" "$i/npd-v2-ql_a.obda" true > "$i/stdout.txt"
-	cp src/main/resources/davide/stats.txt "$i/"
+	java -jar Mixer.jar $DBNAME 10 "$i/npd-v2-ql_a.owl" "$i/npd-v2-ql_a.obda" true > "$i/stdout.txt" # Run the mix 10 times, and put the results in $i/stdout.txt
+
+	# Put the executiion statistics in the $i folder.
+	cp src/main/resources/davide/stats.txt "$i/" 
 	cp src/main/resources/davide/statsMixer.txt "$i"
     done
 }
