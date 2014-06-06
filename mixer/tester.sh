960244856ae9a55599a804bc070c3fbcfebe4e58
tirrolo <tirrolo@users.noreply.github.com>
2014-06-06 18:46:34 +0200

    Update tester.sh
    Edit of tester.sh

--- mixer/tester.sh
+++ mixer/tester.sh
@@ -12,6 +12,7 @@ do_test () {
 	DBNAME=`cat $i/dbName` # Read the database name
 	echo $DBNAME
 	java -jar Mixer.jar $DBNAME 10 "$i/npd-v2-ql_a.owl" "$i/npd-v2-ql_a.obda" true > "$i/stdout.txt" # Run the mix 10 times, and put the results in $i/stdout.txt
+													 # True stands for "Rewriting ON" (Tree-witness Rewriting)
 
 	# Put the executiion statistics in the $i folder.
 	cp src/main/resources/davide/stats.txt "$i/" 
